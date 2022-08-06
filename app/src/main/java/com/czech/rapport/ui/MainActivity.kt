3ab540ba7bcf73c7033cbf353244efea65ed2556
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2022-08-06 18:21:12 +0100

    set up hilt

--- app/src/main/java/com/czech/rapport/ui/MainActivity.kt
+++ app/src/main/java/com/czech/rapport/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.czech.rapport
+package com.czech.rapport.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.czech.rapport.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
